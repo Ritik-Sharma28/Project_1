e134a347436963054e75c3cfd6f9ba8668d53e35
Ritik Sharma <ritiksharma.14@gmail.com>
2025-06-23 15:38:22 +0530

    Add navigation buttons and update imports for First Year components

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,8 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-
+import First_Year from "./components/Years/First_Year/First_Year";
+import Year_Home from "./components/Years/Year_Home/Year_Home";
 function Layout() {
     return ( <>
    <Header/>
--- src/components/Years/First_Year/Mechanical/Mechanical.jsx
+++ src/components/Years/First_Year/Mechanical/Mechanical.jsx
@@ -1,6 +1,14 @@
+import { useNavigate } from "react-router";
 function Mechanical() {
+  const navigate = useNavigate();
     return ( <><div className="p-6 space-y-6 bg-gray-50 text-gray-800">
-  <div className="text-center">
+ <div className="flex items-center gap-10 flex-wrap">
+     <button
+        className=" px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+        onClick={() => navigate(-1)}
+      >
+        ← Back
+      </button>
     <h6 className="text-3xl font-extrabold text-blue-700 uppercase">Mechanical Engineering</h6>
   </div>
 
